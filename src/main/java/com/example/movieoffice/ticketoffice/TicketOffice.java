88d9113fdf68bd20800af277f786f361c08c19b5
wjdghks <wjdghks728@naver.com>
2023-05-20 22:35:00 +0900

    feat: 예매 메서드 파라미터 수정 TheaterType -> String

--- src/main/java/com/example/movieoffice/ticketoffice/TicketOffice.java
+++ src/main/java/com/example/movieoffice/ticketoffice/TicketOffice.java
@@ -5,7 +5,7 @@ import com.example.movieoffice.theater.TheaterType;
 
 public interface TicketOffice {
 //    void setTheater(TheaterType type); // 영화관 종류로 영화관 설정
-    Movie book(TheaterType type, String title); // 영화관 종류와 영화 제목으로 예매
-    void printMovies(TheaterType type); // 특정 영화관의 상영중인 영화 목록 출력
+    Movie book(String theaterName, String title); // 영화관 종류와 영화 제목으로 예매
+    void printMovies(String theaterName); // 특정 영화관의 상영중인 영화 목록 출력
     void printTheaters(); // 영화관 목록 출력
 }
