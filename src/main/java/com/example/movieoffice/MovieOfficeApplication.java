a654ab6745cf31aa54ae0c04f8ca5a70f2076cd8
wjdghks <wjdghks728@naver.com>
2023-05-20 18:04:20 +0900

    Initial commit

---
+++ src/main/java/com/example/movieoffice/MovieOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieoffice/MovieOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
