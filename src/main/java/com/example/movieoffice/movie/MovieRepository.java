e108487a01800ca9de094c988b4ab5a070a83e0a
wjdghks <wjdghks728@naver.com>
2023-05-20 22:35:35 +0900

    feat: 메서드 명 변경 registerMovie -> save getMovie -> retrieve

--- src/main/java/com/example/movieoffice/movie/MovieRepository.java
+++ src/main/java/com/example/movieoffice/movie/MovieRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface MovieRepository {
     List<Movie> getMovies(); // 상영 영화 목록 가져오기
-    void registerMovie(Movie movie); // 영화 추가
+    void save(Movie movie); // 영화 추가
 
-    Movie getMovie(String title); // 제목으로 영화 검색
+    Movie retrieve(String title); // 제목으로 영화 검색
 }
