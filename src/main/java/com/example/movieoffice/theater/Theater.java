e8326f161f9c8b415eb76eb9d55e3d975f010155
wjdghks <wjdghks728@naver.com>
2023-05-20 22:34:09 +0900

    feat: 영화 등록 관련 메서드 추가

--- src/main/java/com/example/movieoffice/theater/Theater.java
+++ src/main/java/com/example/movieoffice/theater/Theater.java
@@ -1,10 +1,21 @@
 package com.example.movieoffice.theater;
 
+import com.example.movieoffice.customer.Customer;
 import com.example.movieoffice.movie.Movie;
+
+import java.util.List;
 
 public interface Theater {
     Movie book(String title); // 제목으로 영화 예매
     void printMovies(); // 상영중인 영화 목록 출력
+    TheaterType getType(); // 영화관 종류 반환
 
-    TheaterType getType();
+//    void join(Customer customer); // 회원 가입
+//
+//    Customer findCustomer(Long customerId); // 회원 검색
+    List<Movie> getMovies(); // 상영 영화 목록 가져오기
+    void registerMovie(Movie movie); // 영화 추가
+
+    Movie findMovie(String title); // 영화 검색
+
 }
