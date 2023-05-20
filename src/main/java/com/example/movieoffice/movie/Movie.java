7f82c43493d74a461271d50d9f602bced8d59693
wjdghks <wjdghks728@naver.com>
2023-05-20 18:17:58 +0900

    feat: Add class

---
+++ src/main/java/com/example/movieoffice/movie/Movie.java
@@ -0,0 +1,7 @@
+package com.example.movieoffice.movie;
+
+public class Movie {
+    String title; // 영화 제목
+    String genre; // 영화 장르
+    Integer runningTime; // 상영 시간
+}
---
+++ src/main/java/com/example/movieoffice/movie/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.example.movieoffice.movie;
+
+import java.util.List;
+
+public interface MovieRepository {
+    List<Movie> getMovies();
+}
