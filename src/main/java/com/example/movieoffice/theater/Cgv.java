713f66bf2368da39461cebe4e50363459cb28dce
wjdghks <wjdghks728@naver.com>
2023-05-20 22:36:28 +0900

    feat: Theater 인터페이스에 추가된 메서드 추가 오버라이딩

--- src/main/java/com/example/movieoffice/theater/Cgv.java
+++ src/main/java/com/example/movieoffice/theater/Cgv.java
@@ -11,20 +11,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Cgv implements Theater {
     private final MovieRepository movieRepository; // 영화들을 가지는 객체
-    private final int price = 10000; // 영화 가격
-
     private final TheaterType type = TheaterType.CGV; // 영화관 종류
+    private final int moviePrice = 10000; // 영화 가격
+
 
     @Override
     public Movie book(String title) {
-        return movieRepository.getMovie(title);
+        return movieRepository.retrieve(title);
     }
 
     @Override
     public void printMovies() {
         List<Movie> movies = movieRepository.getMovies();
         for (Movie movie : movies) {
-            System.out.println(movie);
+            System.out.println("movie = " + movie);
         }
     }
 
@@ -33,4 +33,18 @@ public class Cgv implements Theater {
         return this.type;
     }
 
+    @Override
+    public List<Movie> getMovies() {
+        return movieRepository.getMovies();
+    }
+
+    @Override
+    public void registerMovie(Movie movie) {
+        this.movieRepository.save(movie);
+    }
+
+    @Override
+    public Movie findMovie(String title) {
+        return this.movieRepository.retrieve(title);
+    }
 }
