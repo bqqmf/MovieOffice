77a30ca92e3dddb85c5fc93e69504281f3bf82e3
wjdghks <wjdghks728@naver.com>
2023-05-22 21:30:40 +0900

    type: book 파라미터 추가

--- src/main/java/com/example/movieoffice/ticketoffice/TicketOffice.java
+++ src/main/java/com/example/movieoffice/ticketoffice/TicketOffice.java
@@ -4,7 +4,7 @@ import com.example.movieoffice.movie.Movie;
 
 public interface TicketOffice {
 //    void setTheater(TheaterType type); // 영화관 종류로 영화관 설정
-    Movie book(String theaterName, String title); // 영화관 종류와 영화 제목으로 예매
+    Movie book(String theaterName, Long id, String title); // 영화관 종류와 영화 제목으로 예매
     void printMovies(String theaterName); // 특정 영화관의 상영중인 영화 목록 출력
     void printTheaters(); // 영화관 목록 출력
 }
