7b74a398e7e1c6fe29fd002ceb6cf0c9b0d67f22
wjdghks <wjdghks728@naver.com>
2023-05-22 23:44:17 +0900

    type: 주석 추가

--- src/main/java/com/example/movieoffice/discount/DiscountPolicy.java
+++ src/main/java/com/example/movieoffice/discount/DiscountPolicy.java
@@ -3,5 +3,5 @@ package com.example.movieoffice.discount;
 import com.example.movieoffice.customer.Customer;
 
 public interface DiscountPolicy {
-    int discount(Customer customer, int moviePrice);
+    int discount(Customer customer, int moviePrice); // 할인 후 가격 리턴
 }
