c1693eb5dbe8e224799009344b88649fd4ea44e2
wjdghks <wjdghks728@naver.com>
2023-05-24 00:46:58 +0900

    type: 테스트 코드 수정

--- src/test/java/com/example/movieoffice/ticketoffice/TicketOfficeImplTest.java
+++ src/test/java/com/example/movieoffice/ticketoffice/TicketOfficeImplTest.java
@@ -1,27 +1,38 @@
 package com.example.movieoffice.ticketoffice;
 
 import com.example.movieoffice.Config;
-import com.example.movieoffice.movie.MovieRepository;
+import com.example.movieoffice.movie.Movie;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class TicketOfficeImplTest {
-
     TicketOffice ticketOffice;
 
     @BeforeEach
-    void beforeEach() {
+    void setUp() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);
         ticketOffice = ac.getBean(TicketOffice.class);
-
-
-
     }
 
     @Test
-    void bookAvatarFromCGV() {
-        ticketOffice.book("cgv", 200L, "아바타");
+    void book() {
+        // given
+        String theaterName = "cgv";
+        Long id = 100L;
+        String title = "아바타";
+        String genre = "SF";
+        int runningTime = 180;
+
+        // when
+        Movie bookedMovie = ticketOffice.book(theaterName, id, title);
+
+        // then
+        Assertions.assertThat(bookedMovie).isNotNull();
+        Assertions.assertThat(bookedMovie.getTitle()).isEqualTo(title);
+        Assertions.assertThat(bookedMovie.getGenre()).isEqualTo(genre);
+        Assertions.assertThat(bookedMovie.getRunningTime()).isEqualTo(runningTime);
     }
 }
