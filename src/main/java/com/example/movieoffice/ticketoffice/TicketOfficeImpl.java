ee1c76aa070cdcf1bf1b97f16ff6f23408ac83b7
wjdghks <wjdghks728@naver.com>
2023-05-24 00:40:58 +0900

    type: Map 에서 객체 찾는 로직 수정

--- src/main/java/com/example/movieoffice/ticketoffice/TicketOfficeImpl.java
+++ src/main/java/com/example/movieoffice/ticketoffice/TicketOfficeImpl.java
@@ -22,11 +22,7 @@ private final Map<String, Theater> theaterMap;
 
     @Override
     public void printMovies(String theaterName) {
-        theaterMap.entrySet().stream()
-                .filter(entry -> entry.getValue().getType().name().equalsIgnoreCase(theaterName))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(theaterName + "영화관이 존재하지 않습니다."))
-                .getValue()
+        theaterMap.get(theaterName.toLowerCase())
                 .printMovies();
     }
 
